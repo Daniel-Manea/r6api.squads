5a1a80b82c49e9e6b53c2f49c10459984137c260
Aronel Daniel Manea <danimane4info@gmail.com>
2022-09-27 18:22:16 +0200

    cache node.js update

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,51 @@
 // import { getWeekRecap } from './requests/getWeekRecap';
 import { getMapStats } from './requests/getMapStats';
 import { Platforms } from './config.json';
+import NodeCache from 'node-cache';
+import axios from 'axios';
+const { Ubi_URLS } = require("../config.json");
 
-const Module = async () => {
+var http = require("http");
 
-    const platform = 'uplay';
-    const username = 'squads.es';
+http.createServer(function (request: any, response: any) {
+    // Send the HTTP header 
+    // HTTP Status: 200 : OK
+    // Content Type: text/plain
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
 
-    const MapStats = await getMapStats(username, platform);
+    // Send the response body as "Hello World"
+    response.end('Hello World\n');
+}).listen(8081);
 
-    const RankedAttack = MapStats.platforms[Platforms[platform]].gameModes.ranked.teamRoles.all;
 
-    let mostKillsMap = [];
-    for (let i = 0; i < RankedAttack.length; i++) {
-
-        mostKillsMap.push({ map: RankedAttack[i].statsDetail, kd: RankedAttack[i].killDeathRatio.value });
-
+const getNewToken = async () => {
+    const AxiosConfig = {
+        "Authorization": "Basic a2lnYXlvczk3NEBvdG9kaXIuY29tOkRpc2NvcmQxMjM=",
+        "Content-Type": "application/json",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:104.0) Gecko/20100101 Firefox/104.0",
+        "Accept": "*/*",
+        "Ubi-SessionId": "07b5817b-417a-448a-9825-7bf17c8988a3",
+        "Ubi-AppId": "3587dcbb-7f81-457c-9781-0e3f29f6f56a",
+        "Host": "public-ubiservices.ubi.com",
     }
 
-    console.log(...mostKillsMap)
+    const AuthToken = await axios.post(`${Ubi_URLS.Public}/v3/profiles/sessions`, { "rememberMe": false }, {
+        headers: AxiosConfig
+    })
 
+    return AuthToken.data.ticket;
 }
 
-Module();
+const cache = new NodeCache({ stdTTL: 3600, checkperiod: 10 });
+
+cache.set("token", "GGSFSG", 10);
+
+cache.on("expired", function (key: any, value: any) {
+    console.log(key + " expired")
+    console.log("...getting a new token")
+    const token = getNewToken();
+    cache.set("token", token, 10)
+});
+
+
+
