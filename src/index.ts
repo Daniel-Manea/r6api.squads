b3f49edf023210cc9e9686d0153712d76e1626c5
Aronel Daniel Manea <danimane4info@gmail.com>
2022-09-29 06:52:49 +0200

    deleted open server function

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,6 @@
 import { getMaps } from "./requests/maps/current/getMaps";
 import { getRecap } from "./requests/week/getRecap";
 
-var http = require("http");
-
-http.createServer(function (request: any, response: any) {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.end('Hello World\n');
-}).listen(8081);
-
 export default {
     getMaps,
     getRecap
