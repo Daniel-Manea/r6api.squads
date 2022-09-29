672083a248115a6a27a15009a7a43d260ae7d80c
A. Daniel Manea <76233860+Daniel-Manea@users.noreply.github.com>
2022-09-29 06:58:21 +0200

    Delete node_modules directory

--- node_modules/r6api.js/dist/methods/custom.d.ts
+++
@@ -1,4 +0,0 @@
-import { RequestInit } from 'node-fetch';
-declare const _default: <T>(url: string, params?: Partial<RequestInit>) => Promise<T>;
-export default _default;
-//# sourceMappingURL=custom.d.ts.map
